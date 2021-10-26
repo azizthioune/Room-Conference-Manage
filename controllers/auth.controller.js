810bdbcda9b9572c7feaa8de60a138580cb20e83
Aziz thioune <juniorthioune@gmail.com>
2021-10-26 14:07:01 +0000

    auth endpoints part 1

---
+++ controllers/auth.controller.js
@@ -0,0 +1,44 @@
+const UserModel = require('../models/user.model')
+const jwt = require('jsonwebtoken');
+const { signUpErrors, signInErrors } = require('../utils/error.utils');
+
+const maxAge = 3 * 24 * 60 * 60 * 1000 ;
+
+const createToken = (id) => {
+    return jwt.sign({id}, process.env.TOKEN_SECRET, {
+        expiresIn: maxAge
+    })
+}
+
+module.exports.signUp = async (req, res) => {
+    const {firstname, lastname, nationality, email, password} = req.body
+    console.log(req.body)
+    try {
+        const user = await UserModel.create({firstname, lastname, nationality, email, password});
+        res.status(201).json({user: user._id});
+    }
+    catch (err) {
+        const errors = signUpErrors(err);
+        res.status(200).send({errors});
+    }
+}
+
+module.exports.signIn = async (req, res) => {
+    const { email, password } = req.body
+
+    try {
+        const user = await UserModel.login(email, password);
+        const token = createToken(user._id);
+        res.cookie('jwt', token, { httpOnly: true, maxAge:maxAge });
+        res.status(200).json({user: user._id})
+    } catch (err) {
+        const errors = signInErrors(err);
+        res.status(200).json({errors})
+    }
+}
+
+
+module.exports.logout = async (req, res) => {
+    res.cookie('jwt', '', {maxAge: 1} );
+    res.redirect('/');
+}
--- models/user.model.js
+++ models/user.model.js
@@ -39,7 +39,7 @@ const userSchema = new mongoose.Schema(
             type : String,
             default : "/uploads/profil/random-user.png"
         },
-        likes: {
+        favorites: {
             type: [String]
         }
 
